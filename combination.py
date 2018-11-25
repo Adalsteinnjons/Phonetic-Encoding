5621fb272fd0f7242f5a27ac8670bae76b6d5d76
syarif <syarif.hidayatullah@stud.unibas.ch>
2018-11-25 21:07:03 +0100

    combineAll

---
+++ combination.py
@@ -0,0 +1,36 @@
+def intersection(list1,list2):
+    list3 = list(set(list1) & set(list2))
+    return list3
+
+def union(list1, list2):
+    list3 = list(set(list1 + list2))
+    return list3
+
+def extractWords(path):
+    text = open(path, 'r')
+    text = text.read()
+    text = [s.strip() for s in text.split('\n')]
+    return text
+
+def cleanDuplicate(names):
+    return list(set(names))
+
+def trueRate(names,controlList):
+    return (len(intersection(names,controlList))/len(controlList)) * 100
+
+generatedPath = './cleaningDataset/generatedNames.txt'
+generatedNames = extractWords(generatedPath)
+
+hemming = extractWords('./preprocessed/noTypoLastNames.txt')
+jaccard = extractWords('./preprocessed/jaccardFirstNames.txt')
+levenstein = extractWords('./cleaningDataset/LevenstheinNames.txt')
+soundex = extractWords('./cleaningDataset/SoundexNames.txt')
+
+unionJaccardHemming = union(hemming,jaccard)
+mitLevenStein = union(unionJaccardHemming,levenstein)
+mitSoundex = union(mitLevenStein,soundex)
+final = cleanDuplicate(mitSoundex)
+print(len(final))
+print(trueRate(final,generatedNames))
+
+
